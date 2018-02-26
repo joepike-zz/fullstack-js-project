ac188cb57c3d276226e79d626c24f64727710a94
joepike <joseph.l.pike@gmail.com>
2018-02-26 11:32:27 +0000

    added find function to ZoneController so can now return an array of zones

--- models/Zone.js
+++ models/Zone.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 var ZoneSchema = new mongoose.Schema({
     name: {type:String, default:''},
-    zip: {type:[Number]},
+    zipCode: {type:Array, default:[]},
     timestamp: {type:Date, default:Date.now}
 })
 
