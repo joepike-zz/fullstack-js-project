6cc633761682cfd7931daed53181812277595783
joepike <joseph.l.pike@gmail.com>
2018-02-28 21:19:45 +0000

    completed generic api skeleton for zone and comment resources

--- routes/api.js
+++ routes/api.js
@@ -1,75 +1,73 @@
 var express = require('express')
 var router = express.Router()
 var ZoneController = require('../controllers/ZoneController')
+var controllers = require('../controllers')
 
 router.get('/:resource', function (req, res, next) {
 
     var resource = req.params.resource
+    var controller = controllers[resource]
 
-    if (resource == 'zone') {
-        ZoneController.find(req.query, function (err, results) {
-            if (err) {
-                res.json({
-                    confirmation: 'fail',
-                    message: err
-                })
-
-                return
-            }
-
+    controller.find(req.query, function(err, results){
+        if (err) {
             res.json({
-                confirmation: 'success',
-                results: results
+                confirmation: 'fail',
+                message: err
             })
+
+            return
+        }
+
+        res.json({
+            confirmation: 'success',
+            results: results
         })
-    }
+    })
 })
 
 router.get('/:resource/:id', function (req, res, next) {
 
     var resource = req.params.resource
     var id = req.params.id
+    var controller = controllers[resource]
 
-    if (resource == 'zone') {
-        ZoneController.findById(req.query, function (err, result) {
-            if (err) {
-                res.json({
-                    confirmation: 'fail',
-                    message: err
-                })
-
-                return
-            }
-
+    controller.findById(req.query, function (err, result) {
+        if (err) {
             res.json({
-                confirmation: 'success',
-                result: result
+                confirmation: 'fail',
+                message: err
             })
+
+            return
+        }
+
+        res.json({
+            confirmation: 'success',
+            result: result
         })
-    }
+    })
 })
 
 router.post('/:resource', function (req, res, next) {
 
     var resource = req.params.resource
+    var controller = controllers[resource]
 
-    if (resource == 'zone') {
-        ZoneController.create(req.body, function(err, result) {
-            if(err){
-                res.json({
-                    confirmation: 'fail',
-                    message: err
-                })
-
-                return
-            }
-
+    controller.create(req.body, function(err, result) {
+        if(err){
             res.json({
-                confirmation: 'success',
-                result: result
+                confirmation: 'fail',
+                message: err
             })
+
+            return
+        }
+
+        res.json({
+            confirmation: 'success',
+            result: result
         })
-     }
+    })
 })
 
 module.exports = router
